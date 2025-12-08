b34e31bbda81a08a01dd30c52f1e648e97746f3e
culad-a <alexisdulac.mail@gmail.com>
2025-12-08 18:06:58 +0100

    :speech_balloon: (UTILS) Update pricing logic in getPricePerPerson function

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,10 +6,13 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getPricePerPerson(participants: number): number {
-  if (participants <= 0) return 120; // Default to single person price
-  if (participants === 1) return 120;
-  if (participants === 2) return 72;
-  if (participants >= 3 && participants <= 4) return 60;
-  if (participants >= 5 && participants <= 12) return 55;
+  if (participants <= 0) return 100; // Default to single person price
+  if (participants === 1) return 100;
+  if (participants === 2) return 60;
+  if (participants >= 3 && participants <= 4) return 50;
+  if (participants >= 5 && participants <= 6) return 46;
+  if (participants >= 7 && participants <= 8) return 44;
+  if (participants >= 9 && participants <= 10) return 42;
+  if (participants >= 11 && participants <= 12) return 40;
   throw new Error("Invalid number of participants");
 }
